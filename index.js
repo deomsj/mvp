63e936cc7d4894b1fcf800182e3bf08e2b405940
Jesse DeOms <jesse.deoms@gmail.com>
2017-03-13 13:31:05 -0700

    deployment attempt

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.set('port', process.env.PORT || 3000);
+
 app.get('/', function(req, res) {
   console.log('test');
   res.send('Hello World!');
 });
 
-app.listen(3000, function() {
-  console.log('Algebra Gym App listening on port 3000');
+app.listen(app.get('port'), function() {
+  console.log('Algebra Gym App listening on port: ', app.get('port'));
 });
