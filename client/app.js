19bbec71bd1d02f287f3402273b39af2c5fd16a6
Jesse DeOms <jesse.deoms@gmail.com>
2017-03-13 18:31:54 -0700

    add usser input field and check user answer

--- client/app.js
+++ client/app.js
@@ -1,14 +1,25 @@
 var gym = angular.module('gym', []);
 
-// gym.config(function(){
-
-// });
-
-// gym.run(function(){
-
-// });
-
 gym.controller('PracticeController', function($scope) {
-  $scope.a = Math.ceil(Math.random() * 100);
-  $scope.b = Math.ceil(Math.random() * 100);
+
+  $scope.update = function(){
+    $scope.points = $scope.points || 0;
+    $scope.a = Math.ceil(Math.random() * 100);
+    $scope.b = Math.ceil(Math.random() * 100);
+    $scope.answer = $scope.a + $scope.b;
+  };
+
+  $scope.checkAnswer = function() {
+    if($scope.answer === +$scope.attempt){
+      $scope.display = 'Correct!';
+      $scope.points++;
+      $scope.update();
+    } else{
+      $scope.display = 'Wrong!';
+    }
+    $scope.attempt = '';
+  };
+
+  $scope.update();
+
 });
